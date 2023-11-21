bb792b930993ec65a604792d8f3c701acfb59ef4
haykaza <71695877+haykaza@users.noreply.github.com>
2023-11-21 20:15:04 +0000

    Delete Class Implementation directory

--- Class Implementation/constructRIC.py
+++
@@ -1,8 +0,0 @@
-import refinitiv.dataplatform as rdp
-from defClass.constructFunction import Option_RIC
-
-app_key = open("app_key.txt", "r").read()
-rdp.open_desktop_session(app_key)
-
-ric = Option_RIC('US78378X1072', '2022-02-18', 5000, 'C')
-print(ric.constructRIC())
